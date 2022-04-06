b531daba9baf303d9e1b0048d108a83526671973
TMichael <js5859168@gmail.com>
2022-04-06 21:57:56 +0300

    HumansEntities, знакомство с функциями высшего порядка

--- app/src/main/java/com/inflames1986/humanevolution/MainActivity.kt
+++ app/src/main/java/com/inflames1986/humanevolution/MainActivity.kt
@@ -15,7 +15,32 @@ class MainActivity : AppCompatActivity() {
         human.walk(200)
 
         val walker = Walkable { distance -> Log.d("@@@", "Walkers distance $distance") }
-
         walker.walk(1000)
+
+        val _fun = fun(float: Float): Unit {
+
+        }
+
+        val _fun2 = fun(float: Float): Float {
+            return 1f
+        }
+
+        val _fun3 = fun(boo: Boolean): Boolean {
+            return true
+        }
+
+        fun funhight(
+            _fun: (float: Float) -> Unit,
+            _fun2: (float: Float) -> Float,
+            _fun3: (boo: Boolean) -> Boolean
+        ) { //функции высшего порядка - передача функции внутрь функции
+            val f = _fun(1f)
+            val f2 = _fun2(11f)
+            val f3 = _fun3(true)
+        }
+
+        fun main(){
+            funhight(_fun, _fun2, _fun3 )
+        }
     }
 }
